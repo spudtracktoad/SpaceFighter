f130d93c8500d9045cbc39b03d2642d8d22aba12
CLIR-jared <jared.smith@clirresearch.com>
2021-10-01 21:17:41 -0400

    small changes

--- Assets/Scripts/SpaceshipController.cs
+++ Assets/Scripts/SpaceshipController.cs
@@ -18,6 +18,9 @@ public class SpaceshipController : MonoBehaviour
     public Camera overheadCamera;
 
     float speed;
+    float pitch = 0.0f;
+    float yaw = 0.0f;
+
     Rigidbody r;
     Quaternion lookRotation;
     float rotationZ = 0;
@@ -77,37 +80,42 @@ public class SpaceshipController : MonoBehaviour
         r.velocity = new Vector3(moveDirection.x, moveDirection.y, moveDirection.z);
 
         //Camera follow
-        mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, cameraPosition.position, Time.deltaTime * cameraSmooth);
-        mainCamera.transform.rotation = Quaternion.Lerp(mainCamera.transform.rotation, cameraPosition.rotation, Time.deltaTime * cameraSmooth);
+        yaw += Input.GetAxis("Mouse X") * rotationSpeed;
+        yaw = Mathf.Clamp(yaw, -45, 45);
+        pitch += Input.GetAxis("Mouse Y") * rotationSpeed;
+        pitch = Mathf.Clamp(pitch, -45, 45);
+        spaceCraft.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        //mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, cameraPosition.position, Time.deltaTime * cameraSmooth);
+        //mainCamera.transform.rotation = Quaternion.Lerp(mainCamera.transform.rotation, cameraPosition.rotation, Time.deltaTime * cameraSmooth);
 
-        mouseXSmooth = Mathf.Lerp(mouseXSmooth, Input.GetAxis("Mouse X") * rotationSpeed, Time.deltaTime * cameraSmooth);
-        mouseYSmooth = Mathf.Lerp(mouseYSmooth, Input.GetAxis("Mouse Y") * rotationSpeed, Time.deltaTime * cameraSmooth);
-        Quaternion localRotation = Quaternion.Euler(-mouseYSmooth, mouseXSmooth, -3 * rotationSpeed);
-        lookRotation = lookRotation * localRotation;
-        transform.rotation = lookRotation;
-        rotationZ -= mouseXSmooth;
-        rotationZ = Mathf.Clamp(rotationZ, -45, 45);
+        //mouseXSmooth = Mathf.Lerp(mouseXSmooth, Input.GetAxis("Mouse X") * rotationSpeed, Time.deltaTime * cameraSmooth);
+        //mouseYSmooth = Mathf.Lerp(mouseYSmooth, Input.GetAxis("Mouse Y") * rotationSpeed, Time.deltaTime * cameraSmooth);
+        //Quaternion localRotation = Quaternion.Euler(-mouseYSmooth, mouseXSmooth, -3 * rotationSpeed);
+        //lookRotation = lookRotation * localRotation;
+        //transform.rotation = lookRotation;
+        //rotationZ -= mouseXSmooth;
+        //rotationZ = Mathf.Clamp(rotationZ, -45, 45);
 
         //Rotation
         float rotationZTmp = 0;
-        //if (Input.GetKey(KeyCode.A))
-        //{
-        //    rotationY += -1;
-        //}
-        //else if (Input.GetKey(KeyCode.D))
-        //{
-        //    rotationY += 1;
-        //}
-        //if (Input.GetKey(KeyCode.W))
-        //{
-        //    rotationX += -1;
-        //}
-        //else if (Input.GetKey(KeyCode.S))
-        //{
-        //    rotationX += 1;
-        //}
+        if (Input.GetKey(KeyCode.A))
+        {
+            rotationY += -1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rotationY += 1;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rotationX += -1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rotationX += 1;
+        }
 
-        rotationY += Input.GetAxis("Mouse Y") * rotationSpeed;
+        //rotationY += Input.GetAxis("Mouse Y") * rotationSpeed;
 
         //rotationY = Mathf.Clamp(rotationY, -45, 45);
         //rotationX = Mathf.Clamp(rotationX, -45, 45);
